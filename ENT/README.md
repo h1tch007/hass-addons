53f4caaccb8dff3f135f337d2e416969c4644161
h1tch007 <h1tch@mail.ru>
2025-01-28 12:39:35 +0300

    ent-0.1

---
+++ ENT/README.md
@@ -0,0 +1,22 @@
+# TorrServer
+![Supports amd64 Architecture][amd64-shield]
+## BitTorrent to HTTP streaming
+
+Application for downloading torrents as `HTTP` files.
+Torrent connection is done localy. **If your provider blocks torrents be aware**.
+Cache is stored in `RAM`, that is why internal memory does not corrupting.
+Application consists of two parts, server and client, this is server.
+
+Source of server: https://github.com/YouROK/TorrServer
+
+## Installing
+
+Click on the button below:
+
+[![Open your Home Assistant instance and show the dashboard of a Supervisor add-on.](https://my.home-assistant.io/badges/supervisor_addon.svg)](https://my.home-assistant.io/redirect/supervisor_addon/?addon=765b2bd4_torrserver&repository_url=https%3A%2F%2Fgithub.com%2Fh1tch007%2Fhass-addons)
+
+Or, in your Home Assistant, navigate to
+_Configuration_ > _Add-ons_ > _Add-on Store_
+and search for `TorrServer` in the list.
+
+[amd64-shield]: https://img.shields.io/badge/amd64-yes-green.svg
---
+++ ENT/rootfs/etc/ent/__main__.py
@@ -0,0 +1,90 @@
+import socket
+import yaml
+import json
+from time import sleep
+import paho.mqtt.client as mqtt
+import logging
+
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+    mqtt_config = config.get("mqtt")
+    log_config = config.get("logging")
+    doors = config.get("doors")
+
+def publish_discovery(client, door_id):
+    discovery_topic = f"homeassistant/lock/{door_id}/config"
+    discovery_payload = {
+        "device": {
+            "name": f"ЭНТ Эра-500",
+            "manufacturer": "Эра Новых Технологий",
+            "model": "ЭНТ",
+            "model_id": "Эра-500",
+            "sw_version": "0.24",
+            "connections": [["ip", "10.113.0.100"]]
+        },
+        "name": f"Lock {door_id}",
+        "unique_id": f"era_{door_id}",
+        "command_topic": f"homeassistant/lock/{door_id}/set",
+        "command_template": '{ "action": "{{ value }}", "code": "{{ code }}" }',
+        "state_topic": f"homeassistant/lock/{door_id}/state",
+        "state_locked": "LOCK",
+        "state_unlocked": "UNLOCK",
+        "payload_open": "OPEN",
+        "code_format": "^\\d{4}$",
+    }
+
+    client.publish(discovery_topic, json.dumps(discovery_payload), retain=True)
+    logger.debug(f"MQTT auto discovery payload sent for door {door_id}")
+
+def on_connect(client, userdata, flags, rc, properties):
+    logger.debug("Connected to MQTT Broker with result code "+str(rc))
+    client.subscribe(mqtt_config['MQTT_TOPIC'])
+    
+    # Publish MQTT auto-discovery messages for each lock
+    for door_id in doors:
+        publish_discovery(client, door_id)
+
+def on_message(client, userdata, msg):
+    message = json.loads(msg.payload.decode())
+    room = int(msg.topic.split("/")[2])
+    action = message.get("action")
+    code = message.get("code")
+    delay = int(message.get("delay", 3))
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    if room not in doors:
+        logger.error(f"Room '{room}' is not present in config.yaml")
+        return
+
+    if action == 'UNLOCK' and code == mqtt_config['LOCK_CODE']:
+        sock.sendto(bytes.fromhex(doors[room]['disable_packet']), (doors[room]['host'], 7715))
+        client.publish(f"homeassistant/lock/{room}/state", "UNLOCK")
+        logger.info(f"Lock {room} unlocked.")
+    elif action == 'LOCK' and code == mqtt_config['LOCK_CODE']:
+        sock.sendto(bytes.fromhex(doors[room]['enable_packet']), (doors[room]['host'], 7715))
+        client.publish(f"homeassistant/lock/{room}/state", "LOCK")
+        logger.info(f"Lock {room} locked")
+    elif action == 'OPEN' and code == mqtt_config['LOCK_CODE']:
+        sock.sendto(bytes.fromhex(doors[room]['disable_packet']), (doors[room]['host'], 7715))
+        client.publish(f"homeassistant/lock/{room}/state", "UNLOCK") # Понять почему не работает задержка
+        logger.info(f"Lock {room} unlocking...")
+        sleep(delay) 
+        sock.sendto(bytes.fromhex(doors[room]['enable_packet']), (doors[room]['host'], 7715))
+        client.publish(f"homeassistant/lock/{room}/state", "LOCK")
+        logger.info(f"Lock {room} locked")
+    else: 
+        logger.error("Password is incorrrect.")
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(encoding='utf-8', level=getattr(logging, log_config.get("level")))
+
+# MQTT client setup
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.username_pw_set(mqtt_config['MQTT_USERNAME'], password=mqtt_config['MQTT_PASSWORD'])
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(mqtt_config['MQTT_BROKER'], mqtt_config['MQTT_PORT'], 60)
+logger.debug("Connecting to MQTT Broker...")
+client.loop_forever()
